6deba70e939b5412b5b5588cae50a26dd1da2ccc
xiaohaiH <773884562@qq.com>
2022-10-19 14:22:10 +0800

    :construction: 调整空值函数, 增加通过指定值获取其祖先的函数

--- package/core/utils/index.ts
+++ package/core/utils/index.ts
@@ -13,5 +13,30 @@ export function hasOwn(obj: Record<string, any>, name: string) {
  * @param {*} defaultVal
  */
 export function emptyToValue<T extends unknown>(val: any, defaultVal: T) {
+    if (Array.isArray(val)) return val.filter(Boolean).length ? val : defaultVal;
     return typeof val === 'number' ? val : val || defaultVal;
+}
+
+/**
+ * 获取指定层级的父级(包括自身)
+ * @param {Record<string, any>[]} data 数据源
+ * @param {(item) => boolean} cb 当前数据项是否匹配
+ */
+export function getChained<T extends Record<string, any>>(
+    data: T[],
+    cb: (item: T) => boolean,
+    childrenKey = 'children',
+): T[] {
+    for (const item of data) {
+        if (cb(item)) {
+            return [item];
+        } else if (item[childrenKey]?.length) {
+            const r = getChained(item[childrenKey], cb);
+            if (r.length) {
+                r.unshift(item);
+                return r;
+            }
+        }
+    }
+    return [];
 }
